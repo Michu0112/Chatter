51a0e876a3b58bf2aeda2f9f50044266c576cf32
Michu0112 <michal.pachuc.pl@o2.pl>
2021-05-12 04:01:42 +0200

    established connection

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -20,7 +20,6 @@ signOutBtn.onclick = () => auth.signOut();
 auth.onAuthStateChanged( user =>{
     if(user){
         userGlobal = user.uid;
-        console.log(userGlobal);
         userDeatils.querySelector('.name').innerHTML = `Hello ${user.displayName}!`;
         userDeatils.querySelector('.uid').innerHTML += `User Id: ${user.uid}`;
         signedOutView.hidden = true;
@@ -44,9 +43,34 @@ function sendMessage(e){
         "sender": user,
         "message": message
     });
+
+    document.querySelector('.content__box__inputBox__input').value = '';
 }
 
 const form = document.querySelector('form')
 form.addEventListener('submit', (e) =>{
     sendMessage(e);
 })
+
+//display msgs
+
+firebase.database().ref("messages").on("child_added", function (snapshot){
+
+    let showId;
+    snapshot.val().sender != userGlobal ? showId = 'Chatter' : showId = 'You';
+    let className;
+    snapshot.val().sender != userGlobal ? className = 'author' : className = 'respond';
+
+        let mess = `
+        <li id="message-${snapshot.key}" class="content__box__chat__list__li">
+            <div class="content__box__chat__list__li__wrapper">
+                <span class="content__box__chat__list__${className}">${showId}:  </span>
+                <span> ${snapshot.val().message}</span>
+            </div>
+        </li>
+        `;
+
+
+    document.querySelector('.content__box__chat__list').innerHTML += mess;
+});
+
